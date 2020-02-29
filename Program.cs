704b20d73d8b6df9a300f031c23b5e51abb40ec3
RickBraz <riicardobrazbraz@gmail.com>
2020-02-28 22:24:48 -0300

    adicionei o programa

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Beep();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Não Fala Não pra Mim");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Lá vai minha saudade beber uns Danone");
+            Console.WriteLine("Pra ver se a vontade de te ver some de uma vez da minha cabeça");
+            Console.WriteLine("Eu já 'to vendo a surra de enxaqueca");
+            Console.WriteLine("Lá vai meu orgulho perdendo a vergonha");
+            Console.WriteLine("A comanda enchendo, os dedos tremendo com vontade de ligar");
+            Console.WriteLine("Já 'to com medo de me rejeitar");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Não fala não pra mim, bebê");
+            Console.WriteLine("Senão eu morro de beber");
+            Console.WriteLine("Nessa fossa que eu 'tô, não dá");
+            Console.WriteLine("E é só você pra me salvar");
+            Console.WriteLine("Não fala não pra mim, bebê");
+            Console.WriteLine("Senão eu morro de beber");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Fala que ainda sou o seu amor");
+            Console.WriteLine("Ô Jerry Smith me ajuda, por favor");
+           
+            
+        }
+    }
+}
